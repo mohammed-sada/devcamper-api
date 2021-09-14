11e7f4799eb23d85d7d948c947e38302bdfa253a
mohammed-sada <mohammedabusada94@example.com>
2021-09-14 15:55:46 +0300

    setting up the routes and controllers for bootcamps

---
+++ routes/bootcamps.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const {
+    httpGetBootcamps,
+    httpGetBootcamp,
+    httpCreateBootcamp,
+    httpUpdateBootcamp,
+    httpDeleteBootcamp } = require("../controllers/bootcamps");
+
+const bootcampsRouter = express.Router();
+
+bootcampsRouter.route("/").get(httpGetBootcamps).post(httpCreateBootcamp)
+bootcampsRouter.route("/:id").get(httpGetBootcamp).put(httpUpdateBootcamp).delete(httpDeleteBootcamp);
+
+
+module.exports = bootcampsRouter;
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const morgan = require("morgan");
 
-dotenv.config({ path: "./config/config.env" });
+// Load env vars
+require("dotenv").config({ path: "./config/config.env" });
 
-const PORT = process.env.PORT || 5000;
+// Route files
+const bootcampsRouter = require("./routes/bootcamps")
 
 const app = express();
 
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
-app.listen(PORT, console.log(`server listening in ${process.env.NODE_ENV} mode on port ${PORT} .`))
+// Mount routers
+app.use("/api/v1/bootcamps", bootcampsRouter)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server listening in ${process.env.NODE_ENV} mode on port ${PORT}`))
